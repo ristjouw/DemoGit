bda83f0e02208577e60294d6de3a4cedf9b78a3d
MarkR <gh0125537@windesheim.nl>
2023-04-19 13:28:27 +0200

    Extra klasse

--- src/DemoMain.java
+++ src/DemoMain.java
@@ -2,5 +2,6 @@ public class DemoMain {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Hello World");
+        PrintIets.printIets();
     }
 }
---
+++ src/PrintIets.java
@@ -0,0 +1,6 @@
+public class PrintIets {
+
+    public static void printIets(){
+        System.out.println("Iets");
+    }
+}
